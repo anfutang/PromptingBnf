66a4657bc6e55e363e7dfb45ab80c07e86baaf35
Anfu TANG <tang@gpu.scai.sorbonne-universite.fr>
2024-06-25 13:58:26 +0200

    update

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ Run all prompts on the first subset of queries (1000):
 ```
 sh exp-run-all.sh bnf_p1
 ```
+
+Parse CQs from generated texts and generate a processing log file:
+```
+python3 parse.py --dataset_name bnf_test --prompt_type few-shot --save_as_csv > logs/parse_bnf_test_fs.log
+```
--- opt.py
+++ opt.py
@@ -9,6 +9,7 @@ def get_args():
      parser.add_argument("--model_name",type=str,default="meta-llama/Llama-2-13b-chat-hf")
      parser.add_argument("--prompt_type",type=str)
      parser.add_argument("--batch_size",type=int,default=2,help="batch size used for inference")
+     parser.add_argument("--save_as_csv",action="store_true")
      parser.add_argument("--dry_run",action="store_true")
      args = parser.parse_args()
      return args
